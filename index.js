153d20d38aa4e94978e4ba117f3734fd38d93664
Dominic Gerhauser <dominicgerhauser1@gmail.com>
2023-08-28 20:10:09 +0200

    -

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 // init project
 var express = require('express');
+require("dotenv").config()
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -17,15 +18,39 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
+app.get("/sayhi", (req, res) =>  {
+  res.json({greeting: "Hi"})
+})
+app.get("/api/:date?", (req, res) => {
+  const param = req.params.date
+  date = createDate(param)
+  if(date === undefined) {
+    res.json({error: "Invalid Date"})
+    return 
+  }
+  res.json({unix: date.getTime(), utc: date.toUTCString()})
+})
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
+function createDate(dateString) {
+  console.log(dateString)
+  // current date
+  if (dateString === "" || dateString === undefined || dateString === null) {
+    return new Date()
+  } 
+  let date = new Date(dateString)
+  if(isValidDate(date)) {
+    return date
+  }
+  
+  date =  new Date(parseInt(dateString))
+  if(isValidDate(date)) {
+    return date
+  }
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
+  // unix
+}
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
